b71fbb6512de84dbff63ea22a923a2d70023e101
fizz5700 <785095033@qq.com>
2024-06-17 23:03:17 +0800

    add enemy and hit interfaces

--- ThirdGame/Interfaces/HitInterface.cpp
+++ ThirdGame/Interfaces/HitInterface.cpp
@@ -1,6 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Interfaces/HitInterface.h"
+#include "HitInterface.h"
 
-// Add default functionality here for any IHitInterface functions that are not pure virtual.
